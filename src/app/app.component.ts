5ac17de1d5f6cc87e3af2c176e314703f7fd182c
zhudelong <zhudelong@tiduyun.com>
2017-04-27 16:33:01 +0800

    webpack stylus pug

---
+++ src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+import '../assets/css/styles.css';
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.pug',
+  styleUrls: ['./app.component.styl']
+})
+export class AppComponent { }
